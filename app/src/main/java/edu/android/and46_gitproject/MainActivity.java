9f0fe4d63575cdf54ba20e168ab06e7738205fab
uihyung <powerjuh@gamil.com>
2018-10-19 15:25:20 +0900

    button onClick 추가

--- app/src/main/java/edu/android/and46_gitproject/MainActivity.java
+++ app/src/main/java/edu/android/and46_gitproject/MainActivity.java
@@ -18,5 +18,6 @@ public class MainActivity extends AppCompatActivity {
         textView.setText("Hello Android");
 
         Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(null);
     }
 }
